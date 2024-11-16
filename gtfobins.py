5bde44efc511ec15119efc0ae96c34c8d6dafb86
kenn3y <125267353+kenn3y@users.noreply.github.com>
2024-11-16 20:53:06 +0100

    Add files via upload

--- gtfobins.py
+++ gtfobins.py
@@ -9,30 +9,19 @@ gtfobin = []
 url = 'https://gtfobins.github.io'
 check_suid = []
 response = requests.get(url)
-file = response.text
-regels = file.splitlines()
+regels = response.text.splitlines()
+
+arg_present = len(sys.argv) > 1
 
 for line in regels:
-    if line.strip() != "":
-        if len(sys.argv) == 1:
-            if 'a href' and '#suid' in line:
-                gtfobin.append(line.strip())
-        else:
-            if 'a href' in line:
-                gtfobin.append(line.strip())
+    if 'a href' in line and (not arg_present or '#suid' in line):
+        gtfobin.append(line.strip())
 
 result = subprocess.run(['find','/','-perm','-u=s'], capture_output=True,text=True)
-output = result.stdout
-output_lines = output.splitlines()
-suid_files = {line.split('/')[-1] for line in output_lines}
+suid_files = {line.split('/')[-1] for line in result.stdout.splitlines()}
 
-if len(sys.argv)==1:
-    for suid in sorted(suid_files):
-        for line in gtfobin:
-            #print(line,suid)
-            if suid in line:
-                check_suid.append(suid)
-                break
+if not arg_present:
+    check_suid = sorted({suid for suid in suid_files if any(suid in line for line in gtfobin)})
 
     if check_suid:
         print('[+]Check SUIDS:')
@@ -42,10 +31,9 @@ if len(sys.argv)==1:
     exit(0)
 
 for line in gtfobin:
-    if sys.argv[1] in line:
-        if 'class' in line:
-            regel = line
-            match = re.search(r'href\s*=\s*["\'](.*?)["\']',regel)
-            if match:
-                url2 = match.group(1)
-                print(f'{url}{url2}')
+    if sys.argv[1] in line and 'class' in line:
+        regel = line
+        match = re.search(r'href\s*=\s*["\'](.*?)["\']',regel)
+        if match:
+            url2 = match.group(1)
+            print(f'{url}{url2}')
