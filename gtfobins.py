d8e124dfe2d5518e88ae8ae5979d453a84827bce
kenn3y <125267353+kenn3y@users.noreply.github.com>
2024-11-16 22:01:51 +0100

    Add files via upload

--- gtfobins.py
+++ gtfobins.py
@@ -6,6 +6,7 @@ import sys
 import re
 
 gtfobin = []
+gtfobin2 = []
 url = 'https://gtfobins.github.io'
 check_suid = []
 response = requests.get(url)
@@ -16,6 +17,9 @@ arg_present = len(sys.argv) > 1
 for line in regels:
     if 'a href' in line and (not arg_present or '#suid' in line):
         gtfobin.append(line.strip())
+    elif 'a href' in line and arg_present:
+        gtfobin2.append(line.strip())
+
 
 result = subprocess.run(['find','/','-perm','-u=s'], capture_output=True,text=True)
 suid_files = {line.split('/')[-1] for line in result.stdout.splitlines()}
@@ -30,10 +34,12 @@ if not arg_present:
         print('[-]No matches')
     exit(0)
 
-for line in gtfobin:
-    if sys.argv[1] in line and 'class' in line:
-        regel = line
-        match = re.search(r'href\s*=\s*["\'](.*?)["\']',regel)
-        if match:
-            url2 = match.group(1)
-            print(f'{url}{url2}')
+
+for line in gtfobin2:
+    if sys.argv[1] in line:
+        if 'class' in line:
+            #print(line, sys.argv[1])
+            match = re.search(r'href\s*=\s*["\'](.*?)["\']',line)
+            if match:
+                url2 = match.group(1)
+                print(f'{url}{url2}')   
